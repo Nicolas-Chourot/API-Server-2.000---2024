73de991a0a5dfc4b40a12a31e616a94547cc6585
Nicolas Chourot <Nicolas.Chourot@clg.qc.ca>
2024-11-21 13:00:17 -0500

    first commit

---
+++ wwwroot/Posts/js/siteUI.js
@@ -0,0 +1,507 @@
+const periodicRefreshPeriod = 10;
+let categories = [];
+let selectedCategory = "";
+let currentETag = "";
+let periodic_Refresh_paused = false;
+let postsPanel;
+let itemLayout;
+
+let waiting = null;
+let waitingGifTrigger = 2000;
+let showKeywords = false;
+let minKeywordLenth = 3;
+let keywordsOnchangeTimger = null;
+let keywordsOnchangeDelay = 500;
+
+Init_UI();
+async function Init_UI() {
+    postsPanel = new PageManager('postsScrollPanel', 'postsPanel', 'postSample', renderPosts);
+    $('#createPost').on("click", async function () {
+        showCreatePostForm();
+    });
+    $('#abort').on("click", async function () {
+        showPosts()
+    });
+    $('#aboutCmd').on("click", function () {
+        renderAbout();
+    });
+    $("#showSearch").on('click', function () {
+        toogleShowKeywords();
+        showPosts();
+    });
+
+    installKeywordsOnkeyupEvent();
+    showPosts();
+    start_Periodic_Refresh();
+}
+
+/////////////////////////// Search keywords UI //////////////////////////////////////////////////////////
+function showSearch() {
+    $("#hiddenIcon").hide();
+    $("#showSearch").show();
+    if (showKeywords) {
+        $("#keywordsContainer").show();
+    }
+    else
+        $("#keywordsContainer").hide();
+}
+function hideSearch() {
+    $("#hiddenIcon").show();
+    $("#showSearch").hide();
+    $("#keywordsContainer").hide();
+}
+function toogleShowKeywords() {
+    showKeywords = !showKeywords;
+    if (showKeywords) {
+        $("#keywordsContainer").show();
+        $("#searchKeys").focus();
+    }
+    else {
+        $("#keywordsContainer").hide();
+        showPosts();
+    }
+}
+
+function installKeywordsOnkeyupEvent() {
+    $("#searchKeys").on('keyup', function () {
+        clearTimeout(keywordsOnchangeTimger);
+        keywordsOnchangeTimger = setTimeout(() => { showPosts(); }, keywordsOnchangeDelay);
+    });
+    $("#searchKeys").on('search', function () {
+        showPosts();
+    });
+}
+function cleanSearchKeywords() {
+    /* Keep only keywords of 3 characters or more */
+    let keywords = $("#searchKeys").val().trim().split(' ');
+    let cleanedKeywords = "";
+    keywords.forEach(keyword => {
+        if (keyword.length >= minKeywordLenth) cleanedKeywords += keyword + " ";
+    });
+    $("#searchKeys").val(cleanedKeywords.trim());
+}
+/////////////////////////// Views management ////////////////////////////////////////////////////////////
+async function showPosts() {
+    $("#actionTitle").text("Fil de nouvelles");
+    await postsPanel.show();
+    cleanSearchKeywords();
+    showSearch();
+    $('#abort').hide();
+    $('#form').hide();
+    $('#aboutContainer').hide();
+    $("#createPost").show();
+    $('#confirmPanel').hide();
+    periodic_Refresh_paused = false;
+}
+function hidePosts() {
+    postsPanel.hide();
+    hideSearch();
+    $("#createPost").hide();
+    $("#abort").show();
+    periodic_Refresh_paused = true;
+}
+function renderAbout() {
+    hidePosts();
+    $("#actionTitle").text("À propos...");
+    $("#aboutContainer").show();
+}
+function showCreatePostForm() {
+    renderPostForm();
+}
+async function showEditPostForm(id) {
+    addWaitingGif();
+    let response = await Posts_API.Get(id)
+    if (!Posts_API.error) {
+        let Post = response.data;
+        if (Post !== null)
+            renderPostForm(Post);
+        else
+            renderError("Post introuvable!");
+    } else {
+        renderError(Posts_API.currentHttpError);
+    }
+    removeWaitingGif();
+}
+async function showDeletePostForm(id) {
+    hidePosts();
+    $("#actionTitle").text("Retrait");
+    $('#form').show();
+    $('#form').empty();
+    $('#confirmPanel').show();
+    let response = await Posts_API.Get(id)
+    if (!Posts_API.error) {
+        let post = response.data;
+        if (post !== null) {
+            let date = convertToFrenchDate(UTC_To_Local(post.Date));
+            $("#form").append(`
+                
+                <div class="post" id="${post.Id}">
+                <div class="postHeader">  ${post.Category} </div>
+                <div class="postTitle ellipsis"> ${post.Title} </div>
+                <img class="postImage" src='${post.Image}'/>
+                <div class="postDate"> ${date} </div>
+                <div class="postTextContainer showExtra">
+                    <div class="postText">${post.Text}</div>
+                </div>
+            `);
+            linefeeds_to_Html_br(".postText");
+            // attach form buttons click event callback
+            $('#deletePost').on("click", async function () {
+                await Posts_API.Delete(post.Id);
+                if (!Posts_API.error) {
+                    showPosts();
+                    await postsPanel.update(false);
+                }
+                else {
+                    console.log(Posts_API.currentHttpError)
+                    renderError("Une erreur est survenue!");
+                }
+            });
+            $('#cancel').on("click", function () {
+                showPosts();
+            });
+
+        } else {
+            renderError("Post introuvable!");
+        }
+    } else
+        renderError(Posts_API.currentHttpError);
+}
+
+//////////////////////////// Posts rendering /////////////////////////////////////////////////////////////
+function start_Periodic_Refresh() {
+    setInterval(async () => {
+        if (!periodic_Refresh_paused) {
+            let etag = await Posts_API.HEAD();
+            if (currentETag != etag) {
+                currentETag = etag;
+                await postsPanel.update(false);
+            }
+        }
+    },
+        periodicRefreshPeriod * 1000);
+}
+async function renderPosts(queryString) {
+    let endOfData = false;
+    queryString += "&sort=date,desc";  
+    compileCategories();
+    if (selectedCategory != "") queryString += "&category=" + selectedCategory;
+    if (showKeywords) {
+        let keys = $("#searchKeys").val().replace(/[ ]/g, ',');
+        if (keys !== "")
+            queryString += "&keywords=" + $("#searchKeys").val().replace(/[ ]/g, ',')
+    }
+    addWaitingGif();
+    let response = await Posts_API.Get(queryString);
+    if (!Posts_API.error) {
+        currentETag = response.ETag;
+        let Posts = response.data;
+        if (Posts.length > 0) {
+            Posts.forEach(Post => {
+                postsPanel.itemsPanel.append(renderPost(Post));
+            });
+        } else
+            endOfData = true;
+        linefeeds_to_Html_br(".postText");
+        highlightKeywords();
+        attach_Posts_UI_Events_Callback();
+    } else {
+        renderError(Posts_API.currentHttpError);
+    }
+    removeWaitingGif();
+    return endOfData;
+}
+function renderPost(post) {
+    let date = convertToFrenchDate(UTC_To_Local(post.Date));
+    return $(`
+        <div class="post" id="${post.Id}">
+            <div class="postHeader">
+                ${post.Category}
+                <span class="editCmd cmdIconSmall fa fa-pencil" editPostId="${post.Id}" title="Modifier nouvelle"></span>
+                <span class="deleteCmd cmdIconSmall fa fa-trash" deletePostId="${post.Id}" title="Effacer nouvelle"></span>
+            </div>
+            <div class="postTitle"> ${post.Title} </div>
+            <img class="postImage" src='${post.Image}'/>
+            <div class="postDate"> ${date} </div>
+            <div class="postTextContainer hideExtra">
+                <div class="postText">${post.Text}</div>
+            </div>
+            <div class="postfooter">
+                <span class="moreText cmdIconXSmall fa fa-angle-double-down" title="Afficher la suite"></span>
+                <span class="lessText cmdIconXSmall fa fa-angle-double-up" title="Réduire..."></span>
+            </div>
+        </div>
+    `);
+}
+async function compileCategories() {
+    categories = [];
+    let response = await Posts_API.GetQuery("?fields=category&sort=category");
+    if (!Posts_API.error) {
+        let items = response.data;
+        if (items != null) {
+            items.forEach(item => {
+                if (!categories.includes(item.Category))
+                    categories.push(item.Category);
+            })
+            if (!categories.includes(selectedCategory))
+                selectedCategory = "";
+            updateDropDownMenu(categories);
+        }
+    }
+}
+function updateDropDownMenu() {
+    let DDMenu = $("#DDMenu");
+    let selectClass = selectedCategory === "" ? "fa-check" : "fa-fw";
+    DDMenu.empty();
+    DDMenu.append($(`
+        <div class="dropdown-item menuItemLayout" id="allCatCmd">
+            <i class="menuIcon fa ${selectClass} mx-2"></i> Toutes les catégories
+        </div>
+        `));
+    DDMenu.append($(`<div class="dropdown-divider"></div>`));
+    categories.forEach(category => {
+        selectClass = selectedCategory === category ? "fa-check" : "fa-fw";
+        DDMenu.append($(`
+            <div class="dropdown-item menuItemLayout category" id="allCatCmd">
+                <i class="menuIcon fa ${selectClass} mx-2"></i> ${category}
+            </div>
+        `));
+    })
+    DDMenu.append($(`<div class="dropdown-divider"></div> `));
+    DDMenu.append($(`
+        <div class="dropdown-item menuItemLayout" id="aboutCmd">
+            <i class="menuIcon fa fa-info-circle mx-2"></i> À propos...
+        </div>
+        `));
+    $('#aboutCmd').on("click", function () {
+        renderAbout();
+    });
+    $('#allCatCmd').on("click", function () {
+        selectedCategory = "";
+        showPosts();
+        updateDropDownMenu();
+    });
+    $('.category').on("click", function () {
+        selectedCategory = $(this).text().trim();
+        showPosts();
+        updateDropDownMenu();
+    });
+}
+function attach_Posts_UI_Events_Callback() {
+    linefeeds_to_Html_br(".postText");
+
+    // attach icon command click event callback
+    $(".editCmd").off();
+    $(".editCmd").on("click", function () {
+        showEditPostForm($(this).attr("editPostId"));
+    });
+    $(".deleteCmd").off();
+    $(".deleteCmd").on("click", function () {
+        showDeletePostForm($(this).attr("deletePostId"));
+    });
+    $(".lessText").hide();
+    $(".postfooter").hide();
+
+    // attach text collapse un collapse events callbacks
+    $.each($(".postTextContainer"), function () {
+        let text = $(this).find(">:first-child");
+        if ($(this).innerHeight() < text.outerHeight()) {
+            let postFooter = $(this).parent().find(">:last-child");
+            postFooter.show();
+            let lessText = postFooter.find(">:last-child");
+            lessText.hide();
+        }
+    })
+    $(".moreText").click(function () {
+        let moreText = $(this);
+        let postTextContainer = $(this).parent().parent().find(">:nth-child(5)");
+        postTextContainer.addClass('showExtra');
+        postTextContainer.removeClass('hideExtra');
+        let lessText = moreText.parent().find(">:last-child");
+        moreText.hide();
+        lessText.show();
+    })
+    $(".lessText").click(function () {
+        let lessText = $(this);
+        let postTextContainer = lessText.parent().parent().find(">:nth-child(5)");
+        postTextContainer.addClass('hideExtra');
+        postTextContainer.removeClass('showExtra');
+        let moreText = $(this).parent().find(">:first-child");
+        moreText.show();
+        lessText.hide();
+    })
+}
+function addWaitingGif() {
+    clearTimeout(waiting);
+    waiting = setTimeout(() => {
+        postsPanel.itemsPanel.append($("<div id='waitingGif' class='waitingGifcontainer'><img class='waitingGif' src='Loading_icon.gif' /></div>'"));
+    }, waitingGifTrigger)
+}
+function removeWaitingGif() {
+    clearTimeout(waiting);
+    $("#waitingGif").remove();
+}
+
+/////////////////////// Posts content manipulation ///////////////////////////////////////////////////////
+function linefeeds_to_Html_br(selector) {
+    $.each($(selector), function () {
+        let postText = $(this);
+        var str = postText.html();
+        var regex = /[\r\n]/g;
+        postText.html(str.replace(regex, "<br>"));
+    })
+}
+function highlight(text, elem) {
+    text = text.trim();
+    if (text.length >= minKeywordLenth) {
+        var innerHTML = elem.innerHTML;
+        let startIndex = 0;
+
+        while (startIndex < innerHTML.length) {
+            var normalizedHtml = innerHTML.toLocaleLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+            var index = normalizedHtml.indexOf(text, startIndex);
+            let highLightedText = "";
+            if (index >= startIndex) {
+                highLightedText = "<span class='highlight'>" + innerHTML.substring(index, index + text.length) + "</span>";
+                innerHTML = innerHTML.substring(0, index) + highLightedText + innerHTML.substring(index + text.length);
+                startIndex = index + highLightedText.length + 1;
+            } else
+                startIndex = innerHTML.length + 1;
+        }
+        elem.innerHTML = innerHTML;
+    }
+}
+function highlightKeywords() {
+    if (showKeywords) {
+        let keywords = $("#searchKeys").val().split(' ');
+        if (keywords.length > 0) {
+            keywords.forEach(key => {
+                let titles = document.getElementsByClassName('postTitle');
+                Array.from(titles).forEach(title => {
+                    highlight(key, title);
+                })
+                let texts = document.getElementsByClassName('postText');
+                Array.from(texts).forEach(text => {
+                    highlight(key, text);
+                })
+            })
+        }
+    }
+}
+
+//////////////////////// Forms rendering /////////////////////////////////////////////////////////////////
+
+function newPost() {
+    let Post = {};
+    Post.Id = 0;
+    Post.Title = "";
+    Post.Text = "";
+    Post.Image = "news-logo-upload.png";
+    Post.Category = "";
+    return Post;
+}
+function renderPostForm(post = null) {
+    hidePosts();
+    let create = post == null;
+    if (create) post = newPost();
+    $("#actionTitle").text(create ? "Création" : "Modification");
+    $("#form").show();
+    $("#form").empty();
+    $("#form").append(`
+        <form class="form" id="postForm">
+            <input type="hidden" name="Id" value="${post.Id}"/>
+             <input type="hidden" name="Date" value="${post.Date}"/>
+            <label for="Category" class="form-label">Catégorie </label>
+            <input 
+                class="form-control"
+                name="Category"
+                id="Category"
+                placeholder="Catégorie"
+                required
+                value="${post.Category}"
+            />
+            <label for="Title" class="form-label">Titre </label>
+            <input 
+                class="form-control"
+                name="Title" 
+                id="Title" 
+                placeholder="Titre"
+                required
+                RequireMessage="Veuillez entrer un titre"
+                InvalidMessage="Le titre comporte un caractère illégal"
+                value="${post.Title}"
+            />
+            <label for="Url" class="form-label">Texte</label>
+             <textarea class="form-control" 
+                          name="Text" 
+                          id="Text"
+                          placeholder="Texte" 
+                          rows="9"
+                          required 
+                          RequireMessage = 'Veuillez entrer une Description'>${post.Text}</textarea>
+
+            <label class="form-label">Image </label>
+            <div class='imageUploaderContainer'>
+                <div class='imageUploader' 
+                     newImage='${create}' 
+                     controlId='Image' 
+                     imageSrc='${post.Image}' 
+                     waitingImage="Loading_icon.gif">
+                </div>
+            </div>
+           
+            <div id="keepDateControl">
+                <input type="checkbox" name="keepDate" id="keepDate" class="checkbox" checked>
+                <label for="keepDate"> Conserver la date de création </label>
+                <br>
+            </div>
+            <br>
+            <input type="submit" value="Enregistrer" id="savePost" class="btn btn-primary">
+            <input type="button" value="Annuler" id="cancel" class="btn btn-secondary">
+        </form>
+    `);
+    if (create) $("#keepDateControl").hide();
+    initFormValidation();
+    initImageUploaders();
+    $("#Url").on("change", function () {
+        let favicon = makeFavicon($("#Url").val(), true);
+        $("#faviconLink").empty();
+        $("#faviconLink").attr("href", $("#Url").val());
+        $("#faviconLink").append(favicon);
+    })
+    $('#postForm').on("submit", async function (event) {
+        event.preventDefault();
+        let post = getFormData($("#postForm"));
+        if (post.Category != selectedCategory)
+            selectedCategory = "";
+        if (create || !('keepDate' in post)) post.Date = Local_to_UTC(Date.now());
+        post = await Posts_API.Save(post, create);
+        if (!Posts_API.error) {
+            showPosts();
+            await postsPanel.update(false);
+            postsPanel.scrollToElem(post.Id);
+        }
+        else
+            renderError("Une erreur est survenue!");
+    });
+    $('#cancel').on("click", function () {
+        showPosts();
+    });
+}
+function getFormData($form) {
+    const removeTag = new RegExp("(<[a-zA-Z0-9]+>)|(</[a-zA-Z0-9]+>)", "g");
+    var jsonObject = {};
+    $.each($form.serializeArray(), (index, control) => {
+        jsonObject[control.name] = control.value.replace(removeTag, "");
+    });
+    return jsonObject;
+}
+
+/////////////////// Error rendering /////////////////////////////////////////////////////////////////////
+function renderError(message) {
+    hidePosts();
+    $("#actionTitle").text("Erreur du serveur...");
+    $("#errorContainer").show();
+    $("#errorContainer").append($(`<div>${message}</div>`));
+}
+
